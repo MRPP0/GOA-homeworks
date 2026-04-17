ad2d167a1e39bd9f390351ef3fd68de54dba8ee2
MRPP0 <maniagabrieli4@gmail.com>
2026-04-17 23:54:56 +0400

    first commit

---
+++ day I/lesson 1.py
@@ -0,0 +1,79 @@
+from turtle import *
+
+speed (6.7)
+width ("7")
+color ("green")
+
+forward (250)
+left (90)
+
+forward(250)
+left(90)
+
+forward (250)
+left (90)
+
+forward(250)
+left(90)
+
+forward(80)
+left(90)
+color ("yellow")
+forward (120)
+right(90)
+
+forward(90)
+right(90)
+
+forward(120)
+
+penup()
+goto(250, 250)
+pendown()
+
+color("red")
+right(120)
+
+forward(140)
+left(57)
+
+forward(140)
+
+color("cyan")
+penup()
+goto(50,60)
+pendown()
+
+right(28)
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+right(90)
+forward(30)
+
+penup()
+goto(200,60)
+pendown()
+
+left(90)
+forward(30)
+
+left(90)
+forward(30)
+
+left(90)
+forward(30)
+
+left(90)
+forward(30)
+
+
+exitonclick()
+
+print("by: gabriel mania aka MR.PP")
+print("ngl im hungry i want mcdonalds can i get some?")
+print(" F: 20ნ S: 20მ2 რას უდრის P?")
+print("P=F/S ესეიგი  20n/20მ2=1პა")
+print("now can i get mcd see im smart")
