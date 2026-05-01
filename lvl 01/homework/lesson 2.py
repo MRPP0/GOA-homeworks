a2525eaa02b5d2d20e42d16cb3fcaa3e0d65c27d
MRPP0 <maniagabrieli4@gmail.com>
2026-05-01 17:44:01 +0400

    second commit

---
+++ lvl 01/homework/lesson 2.py
@@ -0,0 +1,31 @@
+#2) თქვენი დავალებაა რომ დაპრინტოთ თქვენი და თქვენი ჯგუფელების სახელები და გვარები და გამოიტანოთ ტერმინალში
+
+name1 = "Andria Mawavariani"
+name2 = "andria kvasilava"
+name3 = "gabriel mania"
+name4 = "luka ustiashvili"
+name5 = "nikoloz salikashvili"
+name6 = "giorgi andriashvili"
+name7 = "dalu manaserovi"
+name8 = "sandro memarnishvili"
+name9 = "tekla kapanaze"
+name10 = "david cogaze"
+name11 = "data boworishvili"
+name12 = "ilia bokuchava"
+name13 = "niko qutatize"
+name14 = "giorgi vartanovi"
+
+print(name1)
+print(name2)
+print(name3)
+print(name4)
+print(name5)
+print(name6)
+print(name7)
+print(name8)
+print(name9)
+print(name10)
+print(name11)
+print(name12)
+print(name13)
+print(name14)
