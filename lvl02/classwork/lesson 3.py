21dc4bf1656efd8836aa4139d377e5cc1ee98a5b
MRPP0 <maniagabrieli4@gmail.com>
2026-05-08 16:37:11 +0400

    second commit

---
+++ lvl02/classwork/lesson 3.py
@@ -0,0 +1,12 @@
+#1) კომენტარების გამოყენებით ახსენით თუ რაში ვიყენებთ კომენტარებს
+#2) კომენტარების გამოყენებით ახსენით თუ რაში ვიყენებთ ცვლადებს მოიყვანეთ ერთი მაგალითი
+#3) შექმენი ორი ცვლადი num1,num2 და თიოთეულში შეინახე სხვადასხვა რიხცვი
+#4) წინა დავალებაში შექმნილი ცვლადების გამოყენებით გააკეთე მათემატიკური ოპერაციები(+ , - , * , / )
+
+
+#we use comment because we can better explain coding to a person who isnt familiar whit coding 
+# we use varibales beacuse we can change bettwen both string and integer example
+
+num1 = 397
+num2 = 87
+print(num1*num2)
---
+++ lvl02/homework/lesson3.py
@@ -0,0 +1,49 @@
+#2) კომენტარის სახით ახსენით თუ რა არიან string ები
+
+#3) კომენტარის სახით ახსენით თუ რა არიან integer ები
+
+#4) კომენტარის სახით ახსენით თუ რა არის მნიშვნელობის გადაწერა
+
+#5) შექმენით 5 ცვლადი სადაც შეინახავთ: თქვენს სახელს, გვარს, ასაკს, დაბადების თარიღს, და საყვარელ სპორტს, შემდეგ კი დაპრინტავთ ყველაფერს ერთად (ცვლადების გარეშე გაკეთებული დავალება არ ჩაითვლება)
+
+#6) გააკეთეთ მნიშვნელობის გადაწერის 2 მაგალითი
+
+#7) შექმენით 2 ცვლადი, num1 და num2, და მოახდინეთ ყველა მათემატიკური ოპერაცია ამ ორ რიცხვს შორის (+, -, *, /)
+
+
+
+
+
+# stringi aris teqsturi monacemi
+# integeri aris ricxviti monacemebi
+# mnisvnelobis gadawera rom vtqvad sxva mnishvnelobas ajlevs cvlads 
+
+
+Name = "Gabriel"
+surname =" Mania"
+age =" age 13"
+birth =" 2013 first of the August"
+favsport = " volleyball"
+
+print(Name + surname + age + birth + favsport)
+
+num= 1
+
+num = 43543
+
+print(num)
+
+num3 = 135
+
+num3 = 6871
+
+print(num3)
+
+num1 = 60
+
+num2 = 5
+
+print(num1+num2)
+print(num1*num2)
+print(num1/num2)
+print(num1-num2)
