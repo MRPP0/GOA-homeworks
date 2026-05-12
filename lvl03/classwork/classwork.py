179c9ac8d93dccc49fc0fb83b5275dcc9493dab1
MRPP0 <maniagabrieli4@gmail.com>
2026-05-12 16:16:13 +0400

    second commit

---
+++ lvl03/classwork/classwork.py
@@ -0,0 +1,18 @@
+#1) ყველა მათემატიკურ ოპერატორზე გააკეთეთ 3 მაგალითი
+#2) კომენტარებით ახსენით თუ რა უნდა გავითვალისწინოთ ცვლადის შექმნისას
+
+
+num1 = 30
+
+num2 = 90 
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 ** num2)
+print(num1 % num2)
+
+# 1) varibels win ar davsvat cifri an simbolo
+# 2) varibles romelic ertze meti winadadebaa space ar unda gavuktod altrantivi aris _
---
+++ lvl03/homework/homework.py
@@ -0,0 +1,42 @@
+#2) კომენტარების სახით ახსენით რამდენი ნაწილისგან შედგება კოდის წერის პროცესი და განმარტეთ თითოეული.
+#
+#3) ივარჯიშეთ და გააკეთეთ მაგალითები მათემატიკურ ოპერატორებზე, განსაკუთრებით დღევანდელ გაკვეთილზე ახსნილ ოპერატორებზე.
+#
+#4) ახსენით რა განმარტებაა / და // გაყოფას შორის.
+#
+#5) კომენტარების სახით ახსენით რა არის case-sensitive და case-insensitive პროგრამირების ენები.
+#
+#6) შექმენით სამი ცვლადი num1, num2 და num3, შემდეგ კი დაპრინტეთ მათი ჯამი, სხვაობა, ნამრავლი, განაყოფი terminal-ში.
+#
+#7) კომენტარების სახით ახსენით რა არის snake_case წერის სტილი და როგორ ხდება ჩანაწერების გაკეთება.
+#
+#8) აუცილებლად უყურეთ გაკვეთილის ჩანაწერს
+
+
+#kodis weris procesi sedgeba ori nawilisgan esenia 1) cvaldis win ar unda iwerebodes aso an nishani
+#2) rodesac cvladi ertze meti sityvit sedgeba space chven ar vaketebdt alternativi aris _
+
+num1 = 420
+
+num2 = 21
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 ** num2)
+print(num1 % num2)
+
+# / ubralod yofs civrs da atwiladi ricxvit gamoyofs cifrs ufro zusti rom iyos
+# // ubralod cifrs wers da atwilads amrgvalebs
+
+# case-sensetive aris programirebis ena romelic yuradexas amaxvilebs did da patara asoebze es nishnavs rom num1 ar aris toli NUM1
+# case-insensetive ar amaxvilebs yuradxebas did da patara asoebze 
+
+num3 = 31
+
+print(num1 + num2 + num3)
+print(num1 - num2 - num3)
+print(num1 / num2 / num3)
+print(num1 * num2 * num3)
