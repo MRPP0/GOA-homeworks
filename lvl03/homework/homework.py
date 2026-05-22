fa5114d4923c54553b367571ed0ffed230e05005
MRPP0 <maniagabrieli4@gmail.com>
2026-05-22 13:45:14 +0400

    second commit

--- lvl03/homework/homework.py
+++ lvl03/homework/homework.py
@@ -40,3 +40,5 @@ print(num1 + num2 + num3)
 print(num1 - num2 - num3)
 print(num1 / num2 / num3)
 print(num1 * num2 * num3)
+
+#snake-case igivea rac _ snake-case aris space chamnacvlebeli
---
+++ lvl04/classwork/classwork.py
@@ -0,0 +1,30 @@
+#1) კომენტარებით ახსენით რა არის input და output და რეალურ ცხოვრებიდან მოიყვანეთ მაგალითი
+#
+#2) კომენტარებით ახსენით თუ რა არის კონკატინაცია და მოიყვანეთ 1 მაგალითი
+#
+#3) input() - ის გამოყენებით მომხმარებელს შემოაყვანინე სახელი და შეინახე ცვლადში
+#
+#4) შექმენი 3 ცვლადი name, lastname, age და შემოაყვანინე თითოელი მნიშვნელობა და შეინახე ცლადში და შემდეგ დაპრინტე შენდეგი წინადადება: გამარჯობა name lastname შენ ხარ age წლის! (აუცილებლად გამოიყენეთ კონკატინაცია)
+
+
+#input aris rodesac raxaca info shegvaq pc shi
+# output shedegi romelic inputis chaweris shemdeg aris
+ 
+
+#konkantacia aris stringebis sheerteba ert stringat mag
+
+name = "joseph"
+
+lastname = "joestar"
+
+print(name + " " + lastname)
+
+name1 = input("eneter your name : ")
+
+lastname2 = input("eneter your lastname : ")
+
+print(name1 + " " + lastname2)
+
+age = input("your age : ")
+
+print("hi my" + " " + "name"+ " " + "is " + " " + name1 + " " + lastname2 + " " +  " " + "and my age is" + " " + age)
