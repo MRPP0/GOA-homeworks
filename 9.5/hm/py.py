5a3fb0ac50f5edfa8cafd94ecca3a28e1a1319f5
MRPP0 <maniagabrieli4@gmail.com>
2026-07-05 16:47:48 +0400

    second commit

---
+++ 9.5/hm/py.py
@@ -0,0 +1,13 @@
+asd = 'mania'
+for i in asd:
+    print(i)
+
+
+for i in range(50,200):
+    print(i)
+
+
+
+
+for i in range(1,100,3):
+    print(i)
---
+++ lvl 09/hm/as.py
@@ -0,0 +1,43 @@
+
+name = "gabriel"
+
+for i in range(100):
+    print(name)
+
+
+#hello = "hello world"  #aq ver gavige
+#
+#for i in hello:
+#    print(hello)  #for loop ის გამოყენებით გამოიტანეთ წინდადება Hello world 1 --> Hello world 100 kitxva
+
+
+num = 0
+
+for i in range(9,100):
+    num = num + i 
+
+print(num)
+    
+
+
+name2 =  'gabriel'
+
+for i in name2:
+    print(i)
+
+
+
+
+
+for i in range(2,1001,2):
+    print(i)
+
+
+
+for i in range(5,500,2):
+    print(i)
+
+
+
+
+
