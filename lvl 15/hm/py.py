ba30d20f725a3bdb4854523782a13e54179209b2
MRPP0 <maniagabrieli4@gmail.com>
2026-08-16 17:35:28 +0400

    second commit

---
+++ lvl 15/hm/py.py
@@ -0,0 +1,26 @@
+#sia aris ramodenime elementis senaxuli collection bradar shio da iwereba ase jer cvladi lsit = ["da ase"]
+
+# index achvenebs rome poziciaze aris is elementi atvla 0 iwyeba
+
+
+name = ['doctor coconut', "shio" , 'jumbera', 'gabriel', 'nikoloza' , 'john pork']
+
+name[0]='john pork'
+
+name[4]='del dough'
+
+
+print(name)
+
+
+
+
+listebi = [69, 420, 67, 9.11, 1212,1212123,987654.1243]
+print(listebi[1])
+print(listebi[3])
+print(listebi[5])
+
+
+
+
+
